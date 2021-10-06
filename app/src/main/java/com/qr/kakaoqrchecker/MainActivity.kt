a4f9a1372c85fc4d16797b9fb7d68d1f1ea42079
wonkyoungChoi <72801820+wonkyoungChoi@users.noreply.github.com>
2021-10-06 13:59:11 +0900

    KakaoQRCheck 어플리케이션 앱 더 가볍게 만들기

--- app/src/main/java/com/qr/kakaoqrchecker/MainActivity.kt
+++ app/src/main/java/com/qr/kakaoqrchecker/MainActivity.kt
@@ -4,18 +4,13 @@ import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.browser.customtabs.CustomTabsIntent
-import com.qr.kakaoqrchecker.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var baseUrl: String
-    private lateinit var viewBinding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        viewBinding = ActivityMainBinding.inflate(layoutInflater)
-        val view = viewBinding.root
-        setContentView(view)
 
         baseUrl = "kakaotalk://qrcheckin?callingPkg=TalkWidgetExtension"
 
