a7922c550e944161b3e9792c94e3536a143ce120
wonkyoungChoi <72801820+wonkyoungChoi@users.noreply.github.com>
2022-01-06 03:04:50 +0900

    KakaoQRCheck 어플리케이션 로고 수정 더욱 간단하게 만들기 카카오 앱과 연동, 없으면 플레이스토어로 이동

--- app/src/main/java/com/qr/kakaoqrchecker/MainActivity.kt
+++ app/src/main/java/com/qr/kakaoqrchecker/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.qr.kakaoqrchecker
 
+import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.browser.customtabs.CustomTabsIntent
 
 class MainActivity : AppCompatActivity() {
@@ -12,11 +14,18 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        baseUrl = "kakaotalk://qrcheckin?callingPkg=TalkWidgetExtension"
+        try {
+            val url = "kakaotalk://qrcheckin?callingPkg=TalkWidgetExtension"
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+            startActivity(intent)
+            finish()
+        } catch (e : Exception) {
+            Toast.makeText(applicationContext, "카카오톡 앱을 설치하셔야 QR인증이 가능합니다.", Toast.LENGTH_SHORT).show()
+            val url = "https://play.google.com/store/apps/details?id=com.kakao.talk"
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+            startActivity(intent)
+            finish()
+        }
 
-        val builder = CustomTabsIntent.Builder()
-        val customTabsIntent = builder.build()
-        customTabsIntent.launchUrl(this, Uri.parse(baseUrl))
-        finish()
     }
 }
